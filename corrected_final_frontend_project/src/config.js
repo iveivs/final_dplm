9fe9e795f307a9068230c0575db50c60b8a4cfa6
Svetoslav <veiv@mail.ru>
2025-02-02 17:17:14 +0300

    fix

--- corrected_final_frontend_project/src/config.js
+++ corrected_final_frontend_project/src/config.js
@@ -1,2 +1,2 @@
-// export const API_HOST = 'http://212.113.117.171:8081';
-export const API_HOST = 'http://localhost:3000';
+export const API_HOST = 'http://212.113.117.171:8081';
+// export const API_HOST = 'http://localhost:3000';
